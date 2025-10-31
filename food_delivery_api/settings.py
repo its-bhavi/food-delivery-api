5d0a8fee53142230eecd6ff263ac80629d36137f
its-bhavi <bigbossbhavya@gmail.com>
2025-10-31 10:58:50 +0530

    Add SQLite fallback for local development database

--- food_delivery_api/settings.py
+++ food_delivery_api/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'food_delivery_api.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        default=os.getenv('DATABASE_URL'),
         conn_max_age=600,
         conn_health_checks=True,
     )
