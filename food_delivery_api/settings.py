6f9c41ecb438594a1bc99d1bc3222a1983f74440
its-bhavi <bigbossbhavya@gmail.com>
2025-11-03 11:50:29 +0530

    Fix CORS configuration - remove invalid wildcard from CORS_ALLOWED_ORIGINS

--- food_delivery_api/settings.py
+++ food_delivery_api/settings.py
@@ -155,10 +155,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # CORS Settings
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",  # Temporary local testing
-    "http://127.0.0.1:8000",
-]
+
 
 CORS_ALLOW_ALL_ORIGINS = True  # Development ke liye - baad mein change karenge
 
