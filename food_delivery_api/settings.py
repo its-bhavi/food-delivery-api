c625ba7621b763a93bd36cb444b973637ba6caeb
its-bhavi <bigbossbhavya@gmail.com>
2025-11-13 12:53:51 +0530

    Add Razorpay payment gateway configuration and dependencies

--- food_delivery_api/settings.py
+++ food_delivery_api/settings.py
@@ -288,3 +288,9 @@ SIMPLE_JWT = {
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+
+# Razorpay Configuration
+RAZORPAY_KEY_ID = os.getenv('RAZORPAY_KEY_ID', 'rzp_test_Rf8SX4fcBCXLU3')
+RAZORPAY_KEY_SECRET = os.getenv('RAZORPAY_KEY_SECRET', 'oAqctnCPvGY1S4u2Uxqo6FR7')
+
+
